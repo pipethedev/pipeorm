93b5534353952eca0101dc5c12a5909dfa82c605
Muritala David <davmuri1414@gmail.com>
2022-10-08 23:25:12 +0100

    perf: started builder

--- Core/Database/Builder.php
+++ Core/Database/Builder.php
@@ -14,4 +14,41 @@ class Builder
     private $offset = '';
     private $limit = '';
     private $commandString = '';
+
+    private $select_command_clauses = array("join", "where", "groupBy", "having", "offset", "limit", "orderBy") ;
+
+    public function table($table): Builder
+    {
+        $this->table = $table;
+        return $this;
+    }
+
+    public function select($select): Builder
+    {
+        if(is_string($select)){
+            if(!in_array($select, $this->select)){
+                $this->select[] = $select;
+            }
+        }else if(is_array($select)){
+            $this->select = $select;
+        }
+        return $this;
+    }
+
+    public function buildSelect()
+    {
+        $command = "";
+        if(count($this->select) == 0){
+             $command = $this->table.'*';
+        }else {
+            $command = implode(",", $this ->select);
+        }
+        $command = "select ".$command." from ".$this->table. " ";
+        foreach ($this->select_command_clauses as $clause){
+            if(!empty($this->$clause)){
+                $command.-$this->$clause. ""
+            }
+        }
+        $command = substr();
+    }
 }
